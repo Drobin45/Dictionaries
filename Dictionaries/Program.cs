c3d018b8bd979f6ebfcaecf6c01c16bec4e17691
drobin45 <drobin45@kent.edu>
2017-07-12 14:17:57 -0400

    Added dictionary practice problems

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -10,6 +10,41 @@ namespace Dictionaries
     {
         static void Main(string[] args)
         {
+            //syntax for dictionary
+
+                ////Dictionary is a class. <string1, string2>- string1 is the key(which will be a string) and string2 will be the value(another string)
+                ////Requires ; after last } of Dictionary.
+
+            //Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            //{
+            //    {"A1", "Twix"}, {"A2", "Twizzler"}, {"A3", "Reeses"}, {"A4", "Sour Patch Kids"}
+            //};
+
+            ////Printing out a full dictionary
+
+            //foreach (KeyValuePair<string, string> item in snackMachine)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+            //drinkMachine.Add(10, "Ginger Ale");
+            //drinkMachine.Add(11, "Bottled Water");
+            //drinkMachine.Add(12, "Orange Juice");
+            //drinkMachine.Add(13, "Fruit Punch");
+            //drinkMachine.Add(14, "Mountain Dew Code Red");
+
+                ////Counting how many items are in your dictionary.
+            //Console.WriteLine(drinkMachine.Count);
+
+                ////Returns all keys
+                ////Returns all values
+            //foreach (KeyValuePair<int, string> drink in drinkMachine)
+            //{
+            //    Console.WriteLine(drink.Key);
+            //    Console.WriteLine(drink.Value);
+            //}          
+            
 
         }
     }
